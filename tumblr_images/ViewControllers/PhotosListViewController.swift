39dff8795a2e59d66e5773bc64eda9944f707341
Nikola Andriiev <ahdpeeb.h@gmail.com>
2018-06-04 19:49:06 +0300

    Extension moved to folder
    AppDelegate removed unused code

---
+++ tumblr_images/ViewControllers/PhotosListViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  tumblr_images
+//
+//  Created by Nikola Andriiev on 6/4/18.
+//  Copyright © 2018 Nikola Andriiev. All rights reserved.
+//
+
+import UIKit
+
+class PhotosListViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
